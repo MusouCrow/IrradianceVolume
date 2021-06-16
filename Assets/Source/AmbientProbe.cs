c21aab7fe7d6de8849387daf279bb21ab9fe190c
MusouCrow <939549924@qq.com>
2021-06-17 01:36:44 +0800

    Making capture.

--- Assets/Source/AmbientProbe.cs
+++ Assets/Source/AmbientProbe.cs
@@ -16,16 +16,19 @@ public class AmbientProbe {
         private set;
     }
 
-    private Camera camera;
     private Color[] colors;
     private RenderTexture renderTexture;
+    private Texture2D texture;
+    private Camera camera;
 
     public AmbientProbe(Vector3 position, float size, int index, Camera camera) {
         this.Position = position;
         this.Size = size;
         this.Index = index;
         this.camera = camera;
-        this.renderTexture = new RenderTexture(1, 1, 0);
+
+        this.renderTexture = new RenderTexture(1, 1, 24, RenderTextureFormat.Default);
+        this.texture = new Texture2D(1, 1, TextureFormat.RGB24, false);
         this.colors = new Color[6];
     }
 
@@ -33,19 +36,27 @@ public class AmbientProbe {
         this.SetColor(0);
     }
 
+    public void Draw() {
+        Gizmos.color = Color.black;
+        var size = new Vector3(this.Size, this.Size, this.Size);
+        Gizmos.DrawWireCube(this.Position, size);
+
+        Gizmos.color = this.colors[0];
+        Gizmos.DrawSphere(this.Position, this.Size * 0.5f);
+    }
+
     public Color GetColor(int index) {
         return this.colors[index];
     }
 
     private void SetColor(int index) {
-        RenderTexture.active = this.renderTexture;
         this.camera.targetTexture = this.renderTexture;
         this.camera.Render();
+        this.camera.targetTexture = null;
         
-        var texture = new Texture2D(1, 1);
-        texture.ReadPixels(new Rect(0, 0, 1, 1), 0, 0);
-        texture.Apply();
-
-        this.colors[index] = texture.GetPixel(0, 0);
+        RenderTexture.active = this.renderTexture;
+        this.texture.ReadPixels(new Rect(0, 0, 1, 1), 0, 0, false);
+        this.colors[index] = this.texture.GetPixel(0, 0);
+        RenderTexture.active = null;
     }
 }
--- Assets/Source/ProbeMgr.cs
+++ Assets/Source/ProbeMgr.cs
@@ -1,14 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class ProbeMgr : MonoBehaviour {
     public Vector3Int size;
     public float interval;
 
+    private List<AmbientProbe> probes;
+    private Vector3Int _size;
+    private float _interval;
+    private Vector3 _position;
+
+    protected void OnRenderObject() {
+        this.FlushProbes();
+    }
+
     protected void OnDrawGizmosSelected() {
-        var pos = this.transform.position;
-        var size = new Vector3(this.interval, this.interval, this.interval);
+        if (this.probes == null) {
+            return;
+        }
 
         Gizmos.color = Color.black;
+
+        foreach (var probe in this.probes) {
+            probe.Draw();
+        }
+    }
+
+    private void FlushProbes() {
+        bool tick = this.size != this._size || this.interval != this._interval || this.transform.position != this._position;
+
+        if (!tick) {
+            return;
+        }
+        
+        this._size = this.size;
+        this._interval = this.interval;
+        this._position = this.transform.position;
+
+        this.probes = new List<AmbientProbe>();
+
+        var camera = this.GetComponentInChildren<Camera>();
+        var pos = this.transform.position;
+        int n = 0;
         
         for (int i = -this.size.x; i <= this.size.x; i++) {
             for (int j = -this.size.y; j <= this.size.y; j++) {
@@ -17,11 +51,14 @@ public class ProbeMgr : MonoBehaviour {
                     p.x += this.interval * i;
                     p.y += this.interval * j;
                     p.z += this.interval * k;
-                    Gizmos.DrawWireCube(p, size);
+                    n++;
+
+                    var probe = new AmbientProbe(p, this.interval, n, camera);
+                    this.probes.Add(probe);
                 }
             }
         }
-        
-        // Gizmos.DrawWireCube()
+
+        this.probes[0].Capture();
     }
 }
